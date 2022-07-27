5bfb81385fe105d0200c188d5b102c0d3e77ede5
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-07-27 19:29:54 +0000

    .

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""
-   This module prints a text with 2 new lines after each of
-   the following characters: '.', '?' and ':'.
-   Raises exception if input is not a string
-"""
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """
-    text_indentation: indent text after these characters and print
-    all text to standard output stream
-    Argument:
-        text (str): multiline string or single line string
+    """Print text with two new lines after each '.', '?', and ':'.
+    Args:
+        text (string): The text to print.
     Raises:
-         TypeError: raises error if text is not a string
+        TypeError: If text is not a string.
     """
-    if type(text) != str:
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    else:
-        for i in range(0, len(text)):
-            # Ommit spaces after punctuation
-            if text[i] == ' ' and (text[i-1] == '?' or text[i-1] == ':'
-               or text[i-1] == '.'):
-                continue
-            print("{}".format(text[i]), end="")
-            # Print 2 new line after punctuation
-            if text[i] == '?' or text[i] == ':' or text[i] == '.':
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
                 print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
