cccea59cb7929b636059477a32d8b05525db79be
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-09-09 11:37:27 +0000

    sql commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
 --- a script that lists all privileges of the MySQL users
-SHOW GRANTS FOR <user_0d_1>@<localhost>;
-SHOW GRANTS FOR <user_0d_2>@<localhost>;
+CREATE USER 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_2'@'localhost';
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
