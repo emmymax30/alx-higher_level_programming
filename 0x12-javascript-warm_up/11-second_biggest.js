2e5076187d6ecf87f4c4ffd2f5abbf26a7f6a02e
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-10-03 22:24:52 -0400

    edit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,29 +1,9 @@
 #!/usr/bin/node
-
-function max (array) {
-  let MaxInt = 0;
-  for (const i of array) {
-    if (i > MaxInt) {
-      MaxInt = i;
-    }
-  }
-  return (MaxInt);
-}
-
-const myarray = [];
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  for (let i = 2; i < process.argv.length; i++) {
-    myarray.push(Number(process.argv[i]));
-  }
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
-const MaxInt = max(myarray);
-for (let i = 0; myarray[i] != null; i++) {
-  if (myarray[i] === MaxInt) {
-    delete myarray[i];
-    break;
-  }
-}
-const MaxInt2 = max(myarray);
-console.log(MaxInt2);
